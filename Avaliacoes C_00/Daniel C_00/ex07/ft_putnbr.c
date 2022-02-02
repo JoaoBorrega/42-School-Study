e8e7a29b9ca0870771d952b6cb46cef641c04ec7
JoaoBorrega <92534643+JoaoBorrega@users.noreply.github.com>
2022-02-02 20:59:08 +0000

    Add files via upload

---
+++ Avaliacoes C_00/Daniel C_00/ex07/ft_putnbr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmoura-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 19:21:18 by dmoura-d          #+#    #+#             */
+/*   Updated: 2022/01/19 12:29:02 by dmoura-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar(45);
+		ft_putchar(50);
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + 48);
+	}
+}
